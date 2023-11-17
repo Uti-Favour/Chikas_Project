171e122701c17b6035845149ad520e73ee808aac
Uti-Favour <utifavour15@gmail.com>
2023-11-17 17:03:21 +0100

    Menu configuration

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
-document.addEventListener('DOMContentLoaded' , function(){
+// JavaScript for toggling mobile menu visibility
+  document.addEventListener('DOMContentLoaded', function () {
+    const mobileMenuButton = document.getElementById('mobile-menu-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+    const menuClosedIcon = document.getElementById('menu-closed-icon');
+    const menuOpenIcon = document.getElementById('menu-open-icon');
 
-  const mobileMenu = document.getElementByIdo('mobile-menu');
-  const menu = document.getElementById('user-menu-button');
-  const userMenuItem0 = document.getElementById('user-menu-item-0');
-  const userMenuItem1 = document.getElementById('user-menu-item-1');
-  const userMenuItem2 = document.getElementById('user-menu-item-2');
+    // Set initial state - menu closed
+    mobileMenuButton.setAttribute('aria-expanded', 'false');
+    mobileMenu.classList.add('hidden');
+    menuClosedIcon.classList.remove('hidden');
+    menuOpenIcon.classList.add('hidden');
 
+    // Function to toggle the mobile menu
+    function toggleMobileMenu() {
+      const expanded = mobileMenuButton.getAttribute('aria-expanded') === 'true' || false;
+      mobileMenuButton.setAttribute('aria-expanded', !expanded);
+      mobileMenu.classList.toggle('hidden');
+      menuClosedIcon.classList.toggle('hidden');
+      menuOpenIcon.classList.toggle('hidden');
+    }
 
-});
+    // Toggle the mobile menu when clicking the image button
+    document.getElementById('logo').addEventListener('click', toggleMobileMenu);
 
+    // Toggle the mobile menu when clicking the menu button with icons
+    mobileMenuButton.addEventListener('click', toggleMobileMenu);
 
+    // Close mobile menu when clicking outside of it
+    document.addEventListener('click', function (event) {
+      if (!mobileMenu.contains(event.target) && !mobileMenuButton.contains(event.target)) {
+        mobileMenuButton.setAttribute('aria-expanded', 'false');
+        mobileMenu.classList.add('hidden');
+        menuClosedIcon.classList.remove('hidden');
+        menuOpenIcon.classList.add('hidden');
+      }
+    });
+  });
 
